c1317c19f3fb9317fb4a9c412a0e0f9b5f8f248f
JackLuoFei <615975238@qq.com>
2018-12-04 15:53:23 +0800

    add moder_1_1.py

---
+++ LXF_Python_Learning/Moder_1/Mode_1_1.py
@@ -0,0 +1,26 @@
+print('I\'m \nok.')
+print('\\\n\\')
+print('\\\t\\')
+print(r'\\\t\\')
+print(r'''Hello,\n
+world.''')
+line1='*'
+line2='**'
+line3='***'
+print(line1,line2,line3,sep='\n')
+print('''line1
+line2
+line3''')
+print(3>5)
+a=123
+print(a)
+a='abc'
+print(a)
+n = 123
+f = 456.789
+s1 = 'Hello,world'
+s2 = 'Hello,\'Adam\''
+s3 = r'Hello,"Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(n,f,s1,s2,s3,s4,sep='\n')
